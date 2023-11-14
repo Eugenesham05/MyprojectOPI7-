de9fb3e11ed5f9efb657de67b856e4a8db92cd7d
Eugene <olia.shamray82@gmail.com>
2023-11-14 15:09:23 +0200

    HArs commit

---
+++ ConsoleApplication17/ConsoleApplication17.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <iomanip>
+#include "windows.h"
+
+using namespace std;
+
+	int sum = 0;
+	for (int i = 1; i < 6; i++)
+	{
+		sum = sum + pow(2, i) * a;
+	}
+	float y;
+	y = 3.56 + sum;
+	return y;
+}
+
+int main()
+{
+	int a, n = 5;
+	cout << "Enter a =";
+	cin >> a;
+	float rez;
+	rez = Sum(a, n);
+	cout << setw(10) << setprecision(3) << "Y= " << rez << endl;
+
+}
